b95d09dfe1192ae7799410f2a1936b81029dfdb0
Jessica <jessica@jmcooper.net>
2022-02-28 17:41:56 -0800

    fixing import errors

--- icairdweakly/models/model_clam.py
+++ icairdweakly/models/model_clam.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from icairdweakly.utils.utils import initialize_weights
+from utils.utils import initialize_weights
 import numpy as np
 
 """
