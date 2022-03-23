f9f718ece62e170d952bd4307267f3f490f5b77c
Jessica <jessica@jmcooper.net>
2022-03-23 12:34:14 +0000

    saliency heatmaps debugging

--- icairdweakly/create_saliency_heatmaps.py
+++ icairdweakly/create_saliency_heatmaps.py
@@ -101,8 +101,8 @@ with h5py.File(args.patch_path, 'r') as f:
                                               interp_mode=args.hipe_interp_mode, verbose=True,
                                               max_depth=args.hipe_max_depth)[0])
 
-        hipe_seg = torch.argmax(torch.cat(hipe_maps, dim=1), dim=1)/num_classes
-        print(np.unique(hipe_seg.numpy()))
+        hipe_seg = torch.argmax(torch.cat(hipe_maps, dim=1), dim=1)
+        print(np.unique(hipe_seg.float().numpy()))
         wandb.log({
             'Patch'.format(i)  : wandb.Image(img, caption=str(logits)),
             'HiPe'             : [wandb.Image(hipe_maps[h], caption=label_list[h]) for h in range(num_classes)],
