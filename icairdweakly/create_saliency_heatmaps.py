fc06e5fb8607d168695671d1e12ad0422c098f61
Jessica <jessica@jmcooper.net>
2022-04-01 08:49:20 +0100

    saliency heatmaps

--- icairdweakly/create_saliency_heatmaps.py
+++ icairdweakly/create_saliency_heatmaps.py
@@ -379,8 +379,9 @@ if __name__ == '__main__':
             x = x - min_x // args.downsample
             y = y - min_y // args.downsample
 
-            print(img.shape, sal_maps.shape)
-            exit()
+            if img.shape[1:] != pdim:
+                img = F.interpolate(img.unsqueeze(0), (pdim, pdim))[0]
+                sal_maps = F.interpolate(sal_maps.unsqueeze(0), (pdim, pdim))[0]
 
             full_img[:, x: x+pdim, y:y+pdim] = img
             full_sal_map[:, x:x+pdim, y:y+pdim] = sal_maps
