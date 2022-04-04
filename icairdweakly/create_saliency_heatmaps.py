64e41877c5c60c9d015315ee75e1d2c411e7d452
Jessica <jessica@jmcooper.net>
2022-04-04 10:21:55 +0100

    saliency heatmaps

--- icairdweakly/create_saliency_heatmaps.py
+++ icairdweakly/create_saliency_heatmaps.py
@@ -76,7 +76,7 @@ def hierarchical_perturbation(model, input, interp_mode='nearest', resize=None, 
             num_cells *= 2
             depth += 1
             if threshold_mode == 'std':
-                threshold = torch.abs(torch.std(torch.mean(saliency, dim=(-1,-2))))
+                threshold = torch.abs(torch.std(torch.max(saliency, dim=(-1,-2))))
             elif threshold_mode == 'mean':
                 threshold = torch.mean(saliency)
             else:
@@ -106,7 +106,8 @@ def hierarchical_perturbation(model, input, interp_mode='nearest', resize=None, 
 
                     if depth > 1:
                         if threshold_mode == 'std':
-                            local_saliency = torch.abs(torch.std(torch.mean(local_saliency, dim=(-1,-2))))
+                            local_saliency = torch.abs(torch.std(torch.max(local_saliency, dim=(-1,-2))))
+                            print(local_saliency)
                         else:
                             local_saliency = torch.max(diff_func(local_saliency))
                     else:
