05b976c340db4161fe603b9d0a2d09f8384f0082
Jessica <jessica@jmcooper.net>
2022-04-11 19:43:29 +0100

    saliency heatmaps

--- icairdweakly/create_saliency_heatmaps.py
+++ icairdweakly/create_saliency_heatmaps.py
@@ -459,7 +459,8 @@ if __name__ == '__main__':
                 scaled_an = torch.sum(F.interpolate(img[:, :, an_x:an_x1, an_y:an_y1], (im_x, im_y)), axis=1)
                 scaled_an[scaled_an != 1.0] = 0.0
 
-                malignant_ss = torch.round(full_sal_map[1])
+                malignant_ss = full_sal_seg
+                malignant_ss[malignant_ss != 1.0] = 0.0
                 malignant_an = scaled_an[0]
                 print(malignant_ss.shape, malignant_an.shape)
                 print(torch.unique(malignant_ss), torch.unique(malignant_an))
