e60858af771ee0b68201be25c1d5ce6ee73d25e4
Jessica <jessica@jmcooper.net>
2022-04-11 19:23:16 +0100

    saliency heatmaps

--- icairdweakly/create_saliency_heatmaps.py
+++ icairdweakly/create_saliency_heatmaps.py
@@ -457,6 +457,7 @@ if __name__ == '__main__':
                 an_x, an_x1, an_y, an_y1 = int(min_x // an_scale_x), int(max_x // an_scale_x), int(
                     min_y // an_scale_y), int(max_y // an_scale_y)
                 scaled_an = torch.sum(F.interpolate(img[:, :, an_x:an_x1, an_y:an_y1], (im_x, im_y)), axis=1)
+                print(torch.unique(scaled_an))
                 wandb.log({'Annotation': wandb.Image(scaled_an)})
                 print(torch.unique(scaled_an, dim=1))
 
