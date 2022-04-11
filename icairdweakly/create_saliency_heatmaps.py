4abcde63c6a67d3c4baf761a48ca632b17d95423
Jessica <jessica@jmcooper.net>
2022-04-11 18:39:17 +0100

    saliency heatmaps

--- icairdweakly/create_saliency_heatmaps.py
+++ icairdweakly/create_saliency_heatmaps.py
@@ -452,8 +452,17 @@ if __name__ == '__main__':
         if len(args.annotation_path) > 0:
             print('Evaluating segmentation performance...')
             with Image.open(args.annotation_path + args.slide_name + "_mask.png") as im:
-                img = to_tensor(im)
-                print(img.shape)
+                img = to_tensor(im).unsqueeze(0)
+
+                an_x, an_y = img.shape[-1], img.shape[-2]
+                an_scale_x, an_scale_y = an_x//(xdim//args.downsample), an_y//(ydim//args.downsample)
+                print(an_scale_x, an_scale_y)
+                an_x, an_x1, an_y, an_y1 = min_x//an_scale_x, max_x//an_scale_x, min_y//an_scale_y, max_y//an_scale_y
+                print(an_x, an_x1, an_y, an_y1)
+                scaled_an = F.interpolate(img[:,:,an_x:an_x1, an_y:an_y1], (an_scale_x, an_scale_y))[0]
+                print(scaled_an.shape)
+                print(full_img.shape)
+
 
 
 
